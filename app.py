73d7501cf4c0254fdb67b50714964e5dc9704f2a
Godchicken <phonlawat1249@gmail.com>
2026-03-01 20:12:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,12 +18,17 @@ client = gspread.authorize(creds)
 
 sheet = client.open_by_key("1FFRK6b1fP1wzr9tqYp7t9PwoDYytIU3GSJIy_SDYpLo").sheet1
 
+print("DEBUG: กำลังจะเรียก add_income")
+
 
 def add_income(amount, note):
     now = datetime.now().strftime("%d/%m/%Y %H:%M")
-    print("DEBUG: กำลังบันทึกลงชีท", now, note, amount)
-    sheet.append_row([now, note, amount])
-    print("DEBUG: บันทึกสำเร็จ")
+    print("DEBUG: เข้า add_income แล้ว")
+    try:
+        sheet.append_row([now, note, amount])
+        print("DEBUG: บันทึกชีทสำเร็จ")
+    except Exception as e:
+        print("ERROR ตอนเขียนชีท:", e)
 
 # ====== ฟังก์ชันเก็บคิวลงไฟล์ ======
 def load_queue():
@@ -154,6 +159,7 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
